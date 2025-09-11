36cd436bc11b625716f7fcbbb7a4c980eb87f0ae
Jana Librova <jcupova@redhat.com>
2025-09-11 10:20:18 +0200

    Add unit test for broken noarch in check_noarch

--- tests/test_check_noarch.py
+++ tests/test_check_noarch.py
@@ -31,9 +31,26 @@ class TestCheckNoarch(unittest.TestCase):
         """
         result_dir = os.path.realpath(__file__)
         dirname = os.path.dirname(result_dir).removesuffix('tests')
-        path_test_source = os.path.join(os.path.dirname(dirname), 'test-source-rpms', 'pytest-rpms')
+        path_test_source = os.path.join(os.path.dirname(dirname), 'test-source-rpms', 'valid-noarch-subpackage')
         sys.argv = ["this", "--results-dir", path_test_source]
         check_noarch()
         actual = self.capsys.readouterr()
         expected = "All noarch rpms matches each other.\n"
         self.assertEqual(expected, actual.out)
+
+    def test_broken_noarch(self):
+        """
+        Test that noarch rpm is broken.
+        """
+        result_dir = os.path.realpath(__file__)
+        dirname = os.path.dirname(result_dir).removesuffix('tests')
+        path_test_source = os.path.join(os.path.dirname(dirname), 'test-source-rpms', 'broken-noarch-subpackage')
+        sys.argv = ["this", "--results-dir", path_test_source]
+        with pytest.raises(SystemExit) as re:
+            check_noarch()
+        assert re.value.code == 1
+        actual = self.capsys.readouterr()
+        expected = "test-noarch-check-noarch-1-1.fc42.noarch.rpm mismatch"
+        self.assertIn(expected, actual.out)
+        expected = "1 errors found"
+        self.assertIn(expected, actual.out)
