1df134f8d5e8ca3d9573c6687f00fd8f63cb5c98
Pavel Raiskup <praiskup@redhat.com>
2025-09-15 14:39:11 +0200

    tests: more extensible file downloader

--- run_tests.sh
+++ run_tests.sh
@@ -5,11 +5,28 @@ args=()
 
 # download test source rpms
 mkdir -p test-source-rpms
-for file in python3-pytest-8.3.5-8.fc44.noarch.rpm pytest-8.3.5-8.fc44.src.rpm; do
-  # skip already downloaded files
-  test -f "test-source-rpms/$file" && continue
-curl "https://github.com/konflux-ci/rpmbuild-pipeline-test-sources/raw/refs/heads/main/$file" --location -o "test-source-rpms/$file"
-done
+
+curl_options=(--location --connect-timeout 60 --retry 3 --retry-delay 10
+    --remote-time --show-error --fail)
+
+cache_url=https://github.com/konflux-ci/rpmbuild-pipeline-test-sources/raw/refs/heads/main/
+
+# $1 directory
+# $@ files to download from GitHub
+download_files()
+{
+    target=$1; shift
+    mkdir -p "$target"
+    for file in "$@"; do
+      base=$(basename "$file")
+      # skip already downloaded files
+      test -f "$target/$file" && continue
+    curl "${curl_options[@]}" "$cache_url/$file" --location -o "$target/$base"
+    done
+}
+
+# Cache those files locally.
+download_files test-source-rpms python3-pytest-8.3.5-8.fc44.noarch.rpm pytest-8.3.5-8.fc44.src.rpm
 
 coverage=( --cov-report term-missing --cov python_scripts )
 for arg; do
